841b8890b4389a87646617f2b9ec7706bd40d8c6
Ionut Moldovan <i.moldovan@giotto.ai>
2024-01-14 23:32:00 +0200

    Auto waiting

---
+++ tests/autoWaiting.spec.ts
@@ -0,0 +1,20 @@
+import {test, expect} from '@playwright/test';
+import { filter } from 'rxjs-compat/operator/filter';
+
+test.beforeEach(async({page}) => {
+    await page.goto('http://uitestingplayground.com/ajax');
+    await page.getByText('Button Triggering AJAX Request').click();
+    });
+
+test('Auto waiting', async({page}) =>{
+    const successButton = page.locator('.bg-success')
+    //await successButton.click()
+
+    //const text = await successButton.textContent()
+    // await successButton.waitFor({state: "attached"})
+    // const text = await successButton.allTextContents()
+
+    // expect(text).toContain('Data loaded with AJAX get request.')
+  
+    await expect(successButton).toHaveText('Data loaded with AJAX get request.', {timeout: 20000})
+})

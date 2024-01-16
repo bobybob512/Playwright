c7ce65ebf93e76039b96c5acbc4a04ee586f0d27
Ionut Moldovan <i.moldovan@giotto.ai>
2024-01-16 10:12:59 +0200

    timeouts

--- tests/autoWaiting.spec.ts
+++ tests/autoWaiting.spec.ts
@@ -1,9 +1,11 @@
 import {test, expect} from '@playwright/test';
 import { filter } from 'rxjs-compat/operator/filter';
+import { timeout } from 'rxjs-compat/operator/timeout';
 
-test.beforeEach(async({page}) => {
+test.beforeEach(async({page}, testInfo) => {
     await page.goto('http://uitestingplayground.com/ajax');
     await page.getByText('Button Triggering AJAX Request').click();
+    testInfo.setTimeout(testInfo.timeout +2000) //this will override timout and will increase the timoeut for every test in this test suite
     });
 
 test('Auto waiting', async({page}) =>{
@@ -33,4 +35,13 @@ test('alternative waits', async({page})=>{
 
     const text = await successButton.allTextContents()
     expect(text).toContain('Data loaded with AJAX get request.')
+})
+
+test('timouts', async({page}) =>{
+    test.setTimeout(10000) //test timoeut
+    test.slow() //increases the test timeout X 3
+    const succesButton = page.locator('.bg-success')
+
+    //await succesButton.click({timeout: 16000}) --action timeout
+    await succesButton.click()
 })

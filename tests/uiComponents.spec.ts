e76ab1e847be9bb7a161784887a7bd2605942681
Ionut Moldovan <i.moldovan@giotto.ai>
2024-01-21 22:43:37 +0200

    checkboxes

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -40,3 +40,25 @@ test.describe('Form layouts page', () => {
         expect(await usingTheGridForm.getByRole('radio', {name: 'Option 2'}).isChecked()).toBeTruthy()
     })
 })
+
+test('checkboxes', async({page}) =>{
+    await page.getByText('Modal & Overlays').click()
+    await page.getByText('Toastr').click()
+
+    //await page.getByRole('checkbox', {name: "Hide on click"}).click({force: true})
+    await page.getByRole('checkbox', {name: "Hide on click"}).check({force: true})
+    await page.getByRole('checkbox', {name: "Hide on click"}).uncheck({force: true})
+
+    await page.getByRole('checkbox', {name: "Prevent arising of duplicate toast"}).check({force: true})
+
+    const allBoxes = page.getByRole('checkbox')
+    for(const box of await allBoxes.all()){
+        await box.check({force: true})
+        expect (await box.isChecked()).toBeTruthy()
+    }
+
+    for(const box of await allBoxes.all()){
+        await box.uncheck({force: true})
+        expect(await box.isChecked()).toBeFalsy()
+    }
+})

6640788ca07ec56f7ee85c229c0d90385fca902b
Ionut Moldovan <i.moldovan@giotto.ai>
2024-01-24 21:22:33 +0200

    tooltip

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -93,3 +93,16 @@ test('lists and dropdowns', async({page}) => {
         }
     }
 })
+
+test('tooltip', async({page}) => {
+    await page.getByText('Modal & Overlays').click()
+    await page.getByText('Tooltip').click()
+
+    const toolTipCard = page.locator('nb-card', {hasText: "Tooltip Placements"})
+    await toolTipCard.getByRole('button', {name: "Top"}).hover()
+
+    page.getByRole('tooltip') // only if you have a role tooltip created
+
+    const tooltip = await page.locator('nb-tooltip').textContent()
+    expect(tooltip).toEqual("This is a tooltip")
+})
